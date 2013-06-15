fad44b3da255fa812848dbe706356523d7975a2e
Hannu Saransaari <hannu.saransaari@gmail.com>
2013-06-15 04:41:14 +0300

    Job system and multi-threading

--- main.cpp
+++ main.cpp
@@ -1,17 +1,61 @@
 #include "SDL.h"
 #include "SDL_opengl.h"
+#include "SDL_thread.h"
 #include "math.hpp"
 #include "image.hpp"
 #include "camera.hpp"
 
 using namespace kd;
 
-static int screen_width = 700;
+static int num_threads   = 3;
+static int screen_width  = 700;
 static int screen_height = 700;
-
+static SDL_sem* sem;
+static SDL_mutex* mutex;
+static Image screen;
 static float demoTime;
 
-static Image screen;
+//
+// Job.
+//
+
+enum JobType
+{
+    RAY_TRACE
+};
+
+struct Job
+{
+    int type;
+    int x, y, w, h;
+    Image* img;
+    const Camera* cam;
+};
+
+static Job jobs[128];
+static int numJobs;
+static int jobsLeft;
+
+static void putJob(Job& j)
+{
+    SDL_mutexP(mutex);
+    jobs[numJobs++] = j;
+    jobsLeft++;
+    SDL_mutexV(mutex);
+    SDL_SemPost(sem);
+}
+
+static bool allJobsDone()
+{
+    SDL_mutexP(mutex);
+    bool done = jobsLeft == 0;
+    SDL_mutexV(mutex);
+    return done;
+}
+
+//
+// Other.
+//
 
 static void putImageFullScreen(const Image& img)
 {
@@ -24,10 +68,10 @@ static float intersectPlane(const Vector3f& o, const Vector3f& d)
     return o.y / -d.y;
 }
 
-void raytrace(Image& dst, const Camera& cam)
+void raytraceSub(Image& dst, const Camera& cam, int sx, int sy, int sw, int sh)
 {
-    for (int y = 0; y < screen.h; y++)
-        for (int x = 0; x < screen.w; x++)
+    for (int y = sy; y < sy+sh; y++)
+        for (int x = sx; x < sx+sw; x++)
         {
             float fy = (y + .5f) / float(screen.h) * 2.f - 1.f;
             float fx = (x + .5f) / float(screen.w) * 2.f - 1.f;
@@ -52,14 +96,14 @@ void raytrace(Image& dst, const Camera& cam)
             {
                 Vector3f p = o + dir * t;
 
-                int xx = int(p.x / 0.2f);
-                int yy = int(p.z / 0.2f);
+                int xx = int(p.x * 5.f);
+                int yy = int(p.z * 5.f);
 
                 Vector4f c;
 
-                c.x = ((xx ^ yy) & 256) / 255.f;
-                c.y = ((xx ^ yy) & 128) / 127.f;
-                c.z = ((xx ^ yy) & 64) / 63.f;
+                c.x = ((xx ^ yy) & 256) * (1.f / 255.f);
+                c.y = ((xx ^ yy) & 128) * (1.f / 127.f);
+                c.z = ((xx ^ yy) & 64) * (1.f / 63.f);
                 c.w = 1.f;
 
                 dst.put(x, y, c);
@@ -67,6 +111,48 @@ void raytrace(Image& dst, const Camera& cam)
             else
                 dst.put(x, y, Vector4f(dir, 1.f));
         }
+}
+
+void raytrace(Image& dst, const Camera& cam)
+{
+    for (int i = 0; i < 16; i++)
+    {
+        int y0 = dst.h * i / 16;
+        int y1 = dst.h * (i+1) / 16;
+
+        Job j;
+        j.type = RAY_TRACE;
+        j.x = 0;
+        j.y = y0;
+        j.w = dst.w;
+        j.h = y1 - y0;
+        j.img = &dst;
+        j.cam = &cam;
+        putJob(j);
+    }
+
+    while (!allJobsDone())
+        SDL_Delay(1);
+}
+
+static int thread_func(void* id)
+{
+    for (;;)
+    {
+        SDL_SemWait(sem);
+        SDL_mutexP(mutex);
+        Job j = jobs[--numJobs];
+        SDL_mutexV(mutex);
+
+        if (j.type == 0)
+            raytraceSub(*j.img, *j.cam, j.x, j.y, j.w, j.h);
+        else
+            assert(0);
+
+        SDL_mutexP(mutex);
+        jobsLeft--;
+        SDL_mutexV(mutex);
+    }
 }
 
 static void render()
@@ -98,7 +184,14 @@ int main(int argc, char* argv[])
     SDL_Init(SDL_INIT_VIDEO);
     SDL_SetVideoMode(screen_width, screen_height, 0, SDL_OPENGL);
 
-    screen.resize(128, 128);
+    screen.resize(256, 256);
+
+    sem = SDL_CreateSemaphore(0);
+    mutex = SDL_CreateMutex();
+
+    SDL_Thread* th[num_threads];
+    for (int i = 0; i < num_threads; i++)
+        th[i] = SDL_CreateThread(thread_func, (void*)i);
 
     for (;;)
     {
@@ -120,6 +213,5 @@ int main(int argc, char* argv[])
         render();
         SDL_GL_SwapBuffers();
     }
-
 }
 
